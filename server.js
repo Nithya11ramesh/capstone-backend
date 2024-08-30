66a16ffc93953b276cfa7769355b271fd725f41e
Nithya11ramesh <nithyanisha06@gmail.com>
2024-08-30 09:26:44 +0530

    commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin: '',
+    origin: 'http://localhost:5173/',
     Credential: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
